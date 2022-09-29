ac9d8f89918b7751fa61fbc1b491179725a900b7
pdoazan <paul.doazan@gmail.com>
2022-09-29 11:02:24 +0200

    Add Drive link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Jour du mariage !
+        <a href="https://drive.google.com/drive/folders/1e42m-ApHGlYJ-1ZskJy8v1ODDQBeTNsC?usp=sharing">Share your pictures</a>
     </div>
   );
 }
