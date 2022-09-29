8c2fb88d355bd274f9409f97f0f6a1365c4450e6
pdoazan <paul.doazan@gmail.com>
2022-09-29 12:48:42 +0200

    Remove Drive link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <a href="https://drive.google.com/drive/folders/1e42m-ApHGlYJ-1ZskJy8v1ODDQBeTNsC?usp=sharing">Share your pictures on Google</a>
         <a href="https://www.dropbox.com/scl/fo/wncnzkgyemjsbz5s42al1/h?dl=0&rlkey=casu45m43yunbrnk7k0dkc0bu">Share your pictures on DropBox</a>
     </div>
   );
