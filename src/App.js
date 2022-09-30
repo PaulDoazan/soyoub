32cc17d79030dc62f8206e938eb3c41302ab1109
pdoazan <paul.doazan@gmail.com>
2022-09-30 22:50:40 +0200

    Add dropbox logo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import React, {useEffect} from "react";
 function App() {
   return (
     <div className="app-container">
+        <div className="link-container">
+            <a href="https://www.dropbox.com/scl/fo/wncnzkgyemjsbz5s42al1/h?dl=0&rlkey=casu45m43yunbrnk7k0dkc0bu"><img className="dropbox-img" src="/dropbox.png"/></a>
+        </div>
         <div className="canvas-container">
             <Collision card_id={1} onlyFirstFrame={false}/>
         </div>
-        <a href="https://www.dropbox.com/scl/fo/wncnzkgyemjsbz5s42al1/h?dl=0&rlkey=casu45m43yunbrnk7k0dkc0bu">Share your pictures on DropBox</a>
     </div>
   );
 }
