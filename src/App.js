84be14f42a6975449f36e126473634d5861896f4
pdoazan <paul.doazan@gmail.com>
2022-10-04 12:50:14 +0200

    Fix png

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app-container">
         <div className="main-container">
-          <img className="speaking" src='/sophiespeaking.png'></img>
+          <img className="speaking" src='/soSpeaking.png'></img>
           <img className="speaking s-down" src='/ayoubSpeaking.png'></img>
           <div className="canvas-container">
               <Collision card_id={1} onlyFirstFrame={false}/>
